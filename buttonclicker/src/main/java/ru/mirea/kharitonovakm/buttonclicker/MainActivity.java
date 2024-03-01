854c00fce043a54d308e24e320800116990cf994
Kristy_27 <71763562+KrUniq@users.noreply.github.com>
2024-03-01 20:21:10 +0300

    Lesson 1

---
+++ buttonclicker/src/main/java/ru/mirea/kharitonovakm/buttonclicker/MainActivity.java
@@ -0,0 +1,41 @@
+package ru.mirea.kharitonovakm.buttonclicker;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.annotation.SuppressLint;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.CheckBox;
+import android.widget.TextView;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+    private TextView textViewStudent;
+    private Button btnWhoAmI;
+    private Button btnItIsNotMe ;
+    private CheckBox checkBox;
+    @SuppressLint("MissingInflatedId")
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        textViewStudent = findViewById(R.id.tvOut);
+        btnWhoAmI = findViewById(R.id.btnWhoAmI);
+        btnItIsNotMe = findViewById(R.id.btnItIsNotMe);
+        View.OnClickListener oclBtnWhoAmI = new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                textViewStudent.setText("Мой номер по списку № 29");
+            }
+        };
+        btnWhoAmI.setOnClickListener(oclBtnWhoAmI);
+
+    }
+    public void onMyButtonClick(View view)
+    {
+        // выводим сообщение
+        Toast.makeText(this, "Это не я сделала.", Toast.LENGTH_SHORT).show();
+        checkBox.setChecked(true);
+    }
+}
